a6dcefde72e7d3f3517de9a66d03dd0865d9e12d
Sam at Laptop <Sam>
2021-03-12 15:42:07 -0500

    Reference Update

--- gym_collision_avoidance/experiments/run_test_experiment.sh
+++ gym_collision_avoidance/experiments/run_test_experiment.sh
@@ -13,49 +13,6 @@ print_header "Running Experiment 1 (Self-play)"
 # Experiment
 cd $DIR
 
-#python3 src/run_experiment_1.py --output_name "exp1_ETH_CADRL" --experiment_num 1 --algorithm_name "CADRL" --experiment_iteration 3 --timeout 10 --dataset_name "ETH"
 
-#python3 src/run_experiment_1.py --output_name "exp1_UNIV_CADRL" --experiment_num 1 --algorithm_name "CADRL" --experiment_iteration 3 --timeout 10 --dataset_name "UNIV"
+python3 src/run_experiment_1.py --output_name "exp4_0.3_CADRL_SOCIALFORCE" --experiment_num 4 --algorithm_name ["CADRL","SOCIALFORCE"] --experiment_iteration 3 --timeout 30 --population_density 0.3
 
-#python3 src/run_experiment_1.py --output_name "exp1_ZARA2_STGCNN" --experiment_num 1 --algorithm_name "STGCNN" --experiment_iteration 3 --timeout 100 --dataset_name "ZARA2"
-
-#python3 src/run_experiment_1.py --output_name "exp1_UNIV_SOCIALFORCE" --experiment_num 1 --algorithm_name "SOCIALFORCE" --experiment_iteration 10 --timeout 60 --dataset_name "UNIV"
-
-
-
-#python3 src/run_experiment_1.py --output_name "exp3_0.3_CADRL_SOCIALFORCE" --experiment_num 3 --algorithm_name ["CADRL","SOCIALFORCE"] --experiment_iteration 2 --timeout 40 --population_density 0.3
-
-#python3 src/run_experiment_1.py --output_name "exp1_ETH_STGCNN" --experiment_num 1 --algorithm_name "STGCNN" --experiment_iteration 3 --timeout 10 --dataset_name "ETH"
-
-#python3 src/run_experiment_1.py --output_name "exp2_0.2_CADRL" --experiment_num 2 --algorithm_name "CADRL" --experiment_iteration 2 --timeout 10 --population_density 0.2
-
-
-
-#python3 src/run_experiment_1.py --output_name "exp2_0.3_CADRL" --experiment_num 2 --algorithm_name "CADRL" --experiment_iteration 2 --timeout 100 --population_density 0.3
-
-#python3 src/run_experiment_1.py --output_name "exp2_0.3_RVO" --experiment_num 2 --algorithm_name "RVO" --experiment_iteration 2 --timeout 100 --population_density 0.3
-
-#python3 src/run_experiment_1.py --output_name "exp2_0.3_LINEAR" --experiment_num 2 --algorithm_name "LINEAR" --experiment_iteration 2 --timeout 100 --population_density 0.3
-
-#python3 src/run_experiment_1.py --output_name "exp2_0.3_SOCIALFORCE" --experiment_num 2 --algorithm_name "SOCIALFORCE" --experiment_iteration 2 --timeout 100 --population_density 0.3
-
-#python3 src/run_experiment_1.py --output_name "exp2_0.2_LINEAR" --experiment_num 2 --algorithm_name "LINEAR" --experiment_iteration 2 --timeout 100 --population_density 0.2
-
-
-####MOTION
-
-#python3 src/run_experiment_1.py --output_name "exp2_0.3_CVM" --experiment_num 2 --algorithm_name "CVM" --experiment_iteration 2 --timeout 50 --population_density 0.3
-
-python3 src/run_experiment_1.py --output_name "exp2_0.3_SPEC" --experiment_num 2 --algorithm_name "SPEC" --experiment_iteration 2 --timeout 10 --population_density 0.3
-
-#python3 src/run_experiment_1.py --output_name "exp3_0.3_CVM_SLSTM" --experiment_num 3 --algorithm_name ["CVM","SLSTM"] --experiment_iteration 2 --timeout 10 --population_density 0.3
-
-
-#python3 src/run_experiment_1.py --output_name "exp4_0.2_CADRL_SPEC" --experiment_num 4 --algorithm_name ["CADRL","SPEC"] --experiment_iteration 3 --timeout 30 --population_density 0.2
-
-#SOCIALGAN STGCNN
-
-#python3 src/run_experiment_1.py --output_name "exp2_0.3_CVM" --experiment_num 2 --algorithm_name "CVM" --experiment_iteration 2 --timeout 20 --population_density 0.3
-
-#TESTING 
-#SPEC SLSTM CVM
